4acab8964a55e86892debdae9a7579ee278ee37e
Blozzem <sperrtmichhd@gmail.com>
2026-07-27 21:03:18 +0200

    fix: install turtle quarry program

--- install.lua
+++ install.lua
@@ -8,7 +8,7 @@ local files = {
   ["startup.lua"] = "/startup",
   ["kernel.lua"] = root .. "/kernel.lua",
   ["ui.lua"] = root .. "/ui.lua",
-  ["quarry.lua"] = root .. "/quarry.lua",
+  ["src/quarry.lua"] = root .. "/quarry.lua",
 }
 
 if not http.checkURL("https://raw.githubusercontent.com") then
